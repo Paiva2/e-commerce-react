f0725175a581cf915f5cb461a1052ea8276b968a
Paiva2 <joaovitor.paiva145@hotmail.com>
2023-02-22 23:14:43 -0300

    first style applied

--- src/App.jsx
+++ src/App.jsx
@@ -7,37 +7,73 @@ function App() {
   const [itensOnPage, setItensOnPage] = useState(10)
   const [currentPage, setCurrentPage] = useState(0)
 
+  useEffect(() =>{    
+    fetch('https://fakestoreapi.com/products')
+    .then(response => response.json())
+    .then(resJSON => setData(resJSON))
+  }, [])
+
   const pages = data ? Math.ceil(data.length / itensOnPage) : undefined
   const initialPage = currentPage * itensOnPage
   const finalPage = initialPage + itensOnPage
   const currentItensOnPage = data ? data.slice(initialPage, finalPage) : undefined
 
   const pageButtons = data
-    ? Array.from(Array(pages), (item, index) => {
+    ? Array.from(Array(pages), (_, index) => {
         return (
           <button
             value={index}
             onClick={(e) => setCurrentPage(+e.target.value)}
-            key={index}
-          >
+            key={index} 
+          > 
             {index + 1}
           </button>
         );
       })
     : undefined;
 
-  useEffect(() =>{
-    fetch('https://fakestoreapi.com/products')
-    .then(response => response.json())
-    .then(resJSON => setData(resJSON))
-  }, [])
-
-
   if(data){
     return (
       <div className="App">
-        {pageButtons}
-        {currentItensOnPage.map(item => <li key={item.id}>{item.title}</li>)}
+        <header className='header'>
+          <section>
+            'Logo'
+          </section>
+          <section>
+          <input type="search" />
+          </section>
+          <section>
+            Wish List
+          </section>
+          <section>
+            Cart
+          </section>
+        </header>
+        <div className='main-container'>
+          {currentItensOnPage.map(item => {
+            return(
+              <div className='product'>
+                <img src={item.image} alt="product" />
+                <p>{item.title}</p>
+                <p>{item.price}</p>
+              </div>
+              )
+            })}
+        </div>
+            <div>
+            {pageButtons}
+            </div>
+            <footer>
+              <div>
+                Social medias
+              </div>
+              <div>
+                Payment Options
+              </div>
+              <div>
+                Terms and About
+              </div>
+            </footer>
       </div>
     )
   }
