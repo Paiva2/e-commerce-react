932e3b5a26211318cb5471c4c63888bd9d3dc77d
Paiva2 <joaovitor.paiva145@hotmail.com>
2023-02-23 21:12:30 -0300

    added restful methods on api

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ server.use(cors(corsOptions));
 
 const products = [
   {
-    id: 1,
+    id: "1",
     name: "Floral Women's Blouse",
     description: "Cotton blouse with floral print",
     price: 49.99,
@@ -20,7 +20,7 @@ const products = [
     rating: 4.5,
   },
   {
-    id: 2,
+    id: "2",
     name: "Men's T-Shirt",
     description: "Classic black t-shirt with white logo",
     price: 29.99,
@@ -28,7 +28,7 @@ const products = [
     rating: 4.2,
   },
   {
-    id: 3,
+    id: "3",
     name: "Women's Jeans",
     description: "Skinny jeans",
     price: 69.99,
@@ -36,7 +36,7 @@ const products = [
     rating: 4.7,
   },
   {
-    id: 4,
+    id: "4",
     name: "Men's Hoodie",
     description: "Soft and comfortable hoodie with drawstring",
     price: 79.99,
@@ -44,7 +44,7 @@ const products = [
     rating: 4.1,
   },
   {
-    id: 5,
+    id: "5",
     name: "Women's Cardigan",
     description: "Knit cardigan with button closure",
     price: 59.99,
@@ -52,7 +52,7 @@ const products = [
     rating: 4.6,
   },
   {
-    id: 6,
+    id: "6",
     name: "Men's Polo Shirt",
     description: "Classic polo shirt with logo",
     price: 39.99,
@@ -60,7 +60,7 @@ const products = [
     rating: 4.0,
   },
   {
-    id: 7,
+    id: "7",
     name: "Women's Denim Skirt",
     description: "A-line denim skirt with button closure",
     price: 49.99,
@@ -68,7 +68,7 @@ const products = [
     rating: 4.3,
   },
   {
-    id: 8,
+    id: "8",
     name: "Men's Chinos",
     description: "Slim-fit chinos in khaki",
     price: 59.99,
@@ -76,7 +76,7 @@ const products = [
     rating: 3.9,
   },
   {
-    id: 9,
+    id: "9",
     name: "Women's Leather Jacket",
     description: "Fitted leather jacket with zipper",
     price: 129.99,
@@ -84,7 +84,7 @@ const products = [
     rating: 4.8,
   },
   {
-    id: 10,
+    id: "10",
     name: "Men's Bomber Jacket",
     description: "Lightweight bomber jacket in navy blue",
     price: 89.99,
@@ -92,7 +92,7 @@ const products = [
     rating: 4.4,
   },
   {
-    id: 11,
+    id: "11",
     name: "Women's Midi Dress",
     description: "Sleeveless midi dress with floral print",
     price: 79.99,
@@ -100,7 +100,7 @@ const products = [
     rating: 4.5,
   },
   {
-    id: 12,
+    id: "12",
     name: "Men's Sweater",
     description: "Cable knit sweater in beige",
     price: 69.99,
@@ -108,7 +108,7 @@ const products = [
     rating: 4.2,
   },
   {
-    id: 13,
+    id: "13",
     name: "Women's Blazer",
     description: "Tailored blazer in black",
     price: 99.99,
@@ -116,7 +116,7 @@ const products = [
     rating: 4.7,
   },
   {
-    id: 14,
+    id: "14",
     name: "Men's Dress Shirt",
     description: "Crisp white dress shirt with French cuffs",
     price: 79.99,
@@ -124,7 +124,7 @@ const products = [
     rating: 4.1,
   },
   {
-    id: 15,
+    id: "15",
     name: "Women's Wide-Leg Pants",
     description: "Flowy pants with high waist and wide legs",
     price: 89.99,
@@ -132,7 +132,7 @@ const products = [
     rating: 4.6,
   },
   {
-    id: 16,
+    id: "16",
     name: "Men's Jogger Pants",
     description: "Comfortable jogger pants with drawstring waist",
     price: 59.99,
@@ -140,7 +140,7 @@ const products = [
     rating: 4.0,
   },
   {
-    id: 17,
+    id: "17",
     name: "Women's Tank Top",
     description: "Sleeveless tank top with lace detail",
     price: 29.99,
@@ -148,7 +148,7 @@ const products = [
     rating: 4.3,
   },
   {
-    id: 18,
+    id: "18",
     name: "Men's Leather Belt",
     description: "Brown leather belt with silver buckle",
     price: 39.99,
@@ -156,7 +156,7 @@ const products = [
     rating: 3.9,
   },
   {
-    id: 19,
+    id: "19",
     name: "Men's Denim Jacket",
     description: "Classic denim jacket with button closure",
     price: 89.99,
@@ -164,7 +164,7 @@ const products = [
     rating: 4.4,
   },
   {
-    id: 20,
+    id: "20",
     name: "Women's Trench Coat",
     description: "Classic beige trench coat with belt",
     price: 149.99,
@@ -173,8 +173,110 @@ const products = [
   },
 ];
 
+const wishList = [
+  {
+    id: "20",
+    name: "Women's Trench Coat",
+    description: "Classic beige trench coat with belt",
+    price: 149.99,
+    image: "https://i.postimg.cc/NMM8QgV4/Women-s-Trench-Coat.jpg",
+    rating: 4.5,
+  },
+  {
+    id: "1",
+    name: "Women's Trench Coat",
+    description: "Classic beige trench coat with belt",
+    price: 149.99,
+    image: "https://i.postimg.cc/NMM8QgV4/Women-s-Trench-Coat.jpg",
+    rating: 4.5,
+  },
+];
+
+const cart = [
+  {
+    id: "20",
+    name: "Women's Trench Coat",
+    description: "Classic beige trench coat with belt",
+    price: 149.99,
+    image: "https://i.postimg.cc/NMM8QgV4/Women-s-Trench-Coat.jpg",
+    rating: 4.5,
+  },
+  {
+    id: "1",
+    name: "Women's Trench Coat",
+    description: "Classic beige trench coat with belt",
+    price: 149.99,
+    image: "https://i.postimg.cc/NMM8QgV4/Women-s-Trench-Coat.jpg",
+    rating: 4.5,
+  },
+];
+
+server.use(express.json());
+
+// Shopping Cart Items
+
+server.get("/cart/:id", (req, res) => {
+  const { id } = req.params;
+  const cart = wishList.filter((product) => product.id === id);
+  return res.json(cart);
+});
+
+server.get("/cart", (req, res) => {
+  return res.json(cart);
+});
+
+server.post("/cart", (request, res) => {
+  const { id, name, price, image, rating } = request.body;
+  const productOnCart = {
+    id: id,
+    name: name,
+    price: price,
+    image: image,
+    rating: rating,
+  };
+  cart.push(productOnCart);
+  return res.json(productOnCart);
+});
+
+// Wish List Items
+
+server.post("/wishlist", (request, res) => {
+  const { id, name, price, image, rating } = request.body;
+  const wishProduct = {
+    id: id,
+    name: name,
+    price: price,
+    image: image,
+    rating: rating,
+  };
+  wishList.push(wishProduct);
+  return res.json(wishProduct);
+});
+
+server.get("/wishlist", (req, res) => {
+  return res.json(wishList);
+});
+
+server.post("/wishlist", (req, res) => {
+  return res.json(wishList);
+});
+
+server.get("/wishlist/:id", (req, res) => {
+  const { id } = req.params;
+  const wishProduct = wishList.filter((product) => product.id === id);
+  return res.json(wishProduct);
+});
+
+// Products from ecommmerce
+
 server.get("/products", (req, res) => {
   return res.json(products);
+});
+
+server.get("/products/:id", (req, res) => {
+  const { id } = req.params;
+  const product = products.filter((product) => product.id === id);
+  return res.json(product);
 });
 
 server.listen(3000, () => {
--- src/App.jsx
+++ src/App.jsx
@@ -1,95 +1,91 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import { useEffect, useState } from 'react'
 import axios from "axios";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faHeart } from '@fortawesome/free-regular-svg-icons'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
-
 import './App.css'
 
 function App() {
-  const [data, setData] = useState()
-  const [itensOnPage, setItensOnPage] = useState(10)
-  const [currentPage, setCurrentPage] = useState(0)
+  const [data, setData] = useState();
+  const [itensOnPage, setItensOnPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(0);
+  const itemProduct = useRef()
+  const pagesArr = []
 
-  useEffect(() =>{
-    callApi();
-  }, [])
+    const pageQuantity = data ? Math.ceil(data.length / itensOnPage) : undefined
+    const initialPage = data ? currentPage * itensOnPage : undefined
+    const finalPage = initialPage + itensOnPage
+    const showItens =  data ? data.slice(initialPage, finalPage) : undefined
 
-  const callApi = () => {
-    axios
-      .get("http://localhost:3000/products")
-      .then((resp) => {
-      setData(resp.data)
-    });
+  for(let i = 0; i < pageQuantity; i++){
+    pagesArr.push(i + 1)
   }
 
+  useEffect(() => {
+    callApi();
+  }, []);
 
-  const pages = data ? Math.ceil(data.length / itensOnPage) : undefined
-  const initialPage = currentPage * itensOnPage
-  const finalPage = initialPage + itensOnPage
-  const currentItensOnPage = data ? data.slice(initialPage, finalPage) : undefined
+  const callApi = () => {
+    axios.get("http://localhost:3000/products").then((resp) => {
+      setData(resp.data);
+    });
+  };
 
-  const pageButtons = data
-    ? Array.from(Array(pages), (_, index) => {
-        return (
-          <button
-            value={index}
-            onClick={(e) => setCurrentPage(+e.target.value)}
-            key={index} 
-          > 
-            {index + 1}
-          </button>
-        );
-      })
-    : undefined;
+  const object = {
+    id: 21,
+    name: "aaaaaaa",
+    description: "aaaaaaaaaaa",
+    price: 9999.99,
+    image: "https://i.postimg.cc/NMM8QgV4/Women-s-Trench-Coat.jpg",
+    rating: 4.5,
+  };
+  const addWishList = (e) => {
+    e.preventDefault();
+    console.log(itemProduct);
+    //axios.post("http://localhost:3000/products", object).then(() => callApi());
+  };
 
-  if(data){
+  if (data) {
     return (
       <div className="App">
-        <header className='header'>
+        <header className="header">
+          <section>'Logo'</section>
           <section>
-            'Logo'
+            <input type="search" />
           </section>
           <section>
-          <input type="search" />
+            <FontAwesomeIcon icon={faHeart} />
           </section>
           <section>
-          <FontAwesomeIcon icon={faHeart} />
-          </section>
-          <section>
-          
-          <FontAwesomeIcon icon={faCartShopping} />
+            <FontAwesomeIcon icon={faCartShopping} />
           </section>
         </header>
-        <div className='main-container'>
-          {currentItensOnPage.map(item => {
-            return(
-              <div key={item.id} className='product'>
+        <div className="main-container">
+          {showItens.map((item) => {
+            return (
+              <div key={item.id} className="product" ref={itemProduct}>
                 <img src={item.image} alt="product" />
-                <p>{item.name} <button><FontAwesomeIcon icon={faHeart} /></button></p>
+                <p>
+                  {item.name}
+                  <button onClick={() => addWishList(item.id)}>
+                    <FontAwesomeIcon icon={faHeart} />
+                  </button>
+                </p>
                 <p>{item.rating}</p>
                 <p>${item.price}</p>
               </div>
-              )
-            })}
+            );
+          })}
         </div>
-            <div>
-            {pageButtons}
-            </div>
-            <footer>
-              <div>
-                Social medias
-              </div>
-              <div>
-                Payment Options
-              </div>
-              <div>
-                Terms and About
-              </div>
-            </footer>
+        <div>{pagesArr.map((item, index) => <button value={currentPage} onClick={() => setCurrentPage(index)}>{item}</button>)}</div>
+        <footer>
+          <div>Social medias</div>
+          <div>Payment Options</div>
+          <div>Terms and About</div>
+        </footer>
       </div>
-    )
+    );
   }
 }
 
